fce6ed1401466d1edfd4e0265f67c0a4cf1c905f
void-echo <beihai142857@outlook.com>
2023-04-28 13:13:31 +0800

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="echo_logger",
+    version="0.0.1",
+    author="Echo Void",
+    author_email="void-echo@outlook.com",
+    description="A simple logger for python, with color and time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+)
