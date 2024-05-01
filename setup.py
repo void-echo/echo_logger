fedec7c71af67e9c4da400f2cf3a2958382afb47
void-echo <Beihai142857@outlook.com>
2024-05-01 22:58:21 +0800

    0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="echo_logger",
-    version="0.1.5",
+    version="0.1.6",
     author="Echo Void",
     author_email="void-echo@outlook.com",
     description="A simple logger for python, with color and time",
@@ -17,5 +17,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        ],
+    ],
+    install_requires=[
+        'requests',
+        'loguru',
+
+    ]
 )
